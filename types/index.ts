2ccf8e2edd6d634e1c0cee1c2a1ae91dfbb918fb
dinesh-swami <dinesh907989@gmail.com>
2026-06-29 16:20:37 +0530

    update more versioning.

--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,4 @@ import { SendEmailSchema, SearchEmailSchema } from '../schemas/email.schema';
 
 export type SendEmailInput = z.infer<typeof SendEmailSchema>;
 export type SearchEmailInput = z.infer<typeof SearchEmailSchema>;
+//export type SearchEmailInput = z.infer<typeof SearchEmailSchema>;
